f650cbfe3bf70489040dc53ef2ed5580e9aebe29
mohamedali7592salem-eng <mohamedali7592salem@gmail.com>
2026-02-20 01:10:38 +0200

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+Enter/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
